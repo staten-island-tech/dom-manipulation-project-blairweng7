b87c4a60c7e4394bdc31c0d744236bbbc2eaf6d2
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2023-10-22 22:37:05 -0400

    apfoneaiof

--- dom.js
+++ dom.js
@@ -1,28 +1,13 @@
 const DOMSelectors = {
   form: document.querySelector("#form"),
-  firstName: document.querySelector(".first-name"),
-  h2s: document.querySelectorAll("h2"),
+  album: document.querySelector('.album'),
+  artist: document.querySelector(".artist"),
+  image: document.querySelector('.image')
 };
-
-DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault();
+DOMSelectors.form.addEventListener("submit", function(event){
+  event.preventDefault
   console.log(DOMSelectors.album.value);
-  DOMSelectors.h2s.forEach(
-    (el) => (el.textContent = DOMSelectors.album.value)
-  );
-});
-
-function makeAlbum() {
-
-}
-
-function addCard (Album) {
-  document.querySelector(".gallery")
-  .insertAdjacentHTML (
-      
-  )
-}
-
-DOMSelectors.form ,addEventListener("submit", function (event) {
-  event.preventDefault();
+    DOMSelectors.h2s.forEach(
+      (el) => (el.textContent = DOMSelectors.album.value)
+    );
 })
---
+++ notes.js
@@ -0,0 +1,28 @@
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+    firstName: document.querySelector(".first-name"),
+    h2s: document.querySelectorAll("h2"),
+  };
+  
+  DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(DOMSelectors.album.value);
+    DOMSelectors.h2s.forEach(
+      (el) => (el.textContent = DOMSelectors.album.value)
+    );
+  });
+  
+  function makeAlbum() {
+  
+  }
+  
+  function addCard (Album) {
+    document.querySelector(".gallery")
+    .insertAdjacentHTML (
+        
+    )
+  }
+  
+  DOMSelectors.form ,addEventListener("submit", function (event) {
+    event.preventDefault();
+  })
