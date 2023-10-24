382a8a2c2f8a708e948d9faf4fb06cfe5c2c940f
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2023-10-24 09:24:51 -0400

    AHHHHHHHSEHIOFAIEHNIOBFIA

--- dom.js
+++ dom.js
@@ -1,13 +1,27 @@
+const form = document.querySelector("#form");
+console.log(form)
 const DOMSelectors = {
   form: document.querySelector("#form"),
-  album: document.querySelector('.album'),
-  artist: document.querySelector(".artist"),
-  image: document.querySelector('.image')
+  album: document.querySelector("album"),
+  artist: document.querySelector("artist"),
+  image: document.querySelector("image"),
+  h2s: document.querySelectorAll("h2"),
+  gallery: document.querySelector("gallery")
 };
+
+function backgroundandtext(background, text) {
+  background.style.backgroundColor = "pink"
+  text.textContent
+}
+DOMSelectors.gallery.insertAdjacentHTML(
+  "beforeend"
+)
 DOMSelectors.form.addEventListener("submit", function(event){
-  event.preventDefault
-  console.log(DOMSelectors.album.value);
-    DOMSelectors.h2s.forEach(
-      (el) => (el.textContent = DOMSelectors.album.value)
-    );
-})
+  event.preventDefault();
+  console.log(DOMSelectors.album.value,);
+    DOMSelectors.h2s.forEach((el) => el.textContent = DOMSelectors.album.value)
+});
+
+function makeAlbum() {
+
+}
